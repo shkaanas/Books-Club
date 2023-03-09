3ffc7b6ad87281754b069af12b6fd4df3b0232d6
Anastasia <nastashkirta@gmail.com>
2023-03-09 21:06:41 +0200

    added header

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Container from '@mui/material/Container';
-
+import heroPicture from '../images/hero-picture.png';
 
 export default function Home() {
   return (
     <Container maxWidth="xl">
-      <h1>Hello</h1>
-      <p>lorem ipsum</p>
-      <p className="text-3xl font-bold underline">Home</p>
+      <header className="layout">
+        <div className=' layout layout_alt'>
+          <h2 className='heading'>Books for your heart and soul</h2>
+          <p className='heading heading_alt'>Some text here, even don’t know what to write</p>
+          <button className='btn btn_custom'>Show me</button>
+        </div>
+        <div>
+          <img src={heroPicture} alt='hero image' className='image'/>
+        </div>
+      </header>
     </Container>
   );
 }
